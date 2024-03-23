a2cf2a32da273a5561492e1d648cbb6485d3910c
ElizaVaz <150373929+ElizaVaz@users.noreply.github.com>
2024-03-23 15:28:05 +0300

    Update Live.py
    +world

--- Live.py
+++ Live.py
@@ -11,4 +11,27 @@ while not n_print.isdigit():  # Проверка что ввели число
     n_print = input("Длина 'мира': ")
 n_print = int(n_print)
 
-world = ["o"] * len_world
+print("Теперь введите изначальный мир.")
+print("Если вы хотите чтобы в клетке не было жизни введите 0, иначе любую другую цыфру.")
+while True:
+    world = input("Введите 'изначальный мир': ")
+    if len(world) != len_world:
+        if len(world) < len_world:
+            print("Не достаточно символов для мира...")
+        else:
+            print("Слишком много символов для мира...")
+        continue
+    if not world.isdigit():
+        print(f"Ты уверен что здесь только цыфры {world}?")
+        continue
+    break
+
+world_x = list()
+for i in range(len_world):
+    if world[i] == "0":
+        world_x.append("o")
+    else:
+        world_x.append(" ")
+world = world_x
+
+print("ИЗНАЧАЛЬНЫЙ МИР:", "".join([x for x in world]))
